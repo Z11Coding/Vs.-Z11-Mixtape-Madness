ae1ae93c0bba8f8403db9fcc41c46ba7bcf7891a
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2024-07-02 09:58:22 -0400

    clearing out unfinished things
    ill re-add them once they have sprites done for them

--- assets/shared/data/cant-resist/Healthstuff.lua
+++
@@ -1,5 +0,0 @@
-function opponentNoteHit(id, direc, type, sus)
-if not (type == 'GF Sing' or gfSection) then
-setProperty('health', math.max(getProperty('health') - 0.01, 0.0000001))
-end
-end
--- assets/shared/data/cant-resist/Wraparound Note (Behind).lua
+++
@@ -1,9 +0,0 @@
-function onUpdatePost(elapsed)
-    for a = 0, getProperty('notes.length') - 1 do
-        if getPropertyFromGroup('notes', a, 'noteType') == 'Wraparound Note (Behind)' then
-setPropertyFromGroup('notes', a, 'multSpeed', math.max(getPropertyFromGroup('notes', a, 'multSpeed') - 0.010, -0.2))
-
-    end
-end	
-end
-
