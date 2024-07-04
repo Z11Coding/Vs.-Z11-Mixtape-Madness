919f362474ee1d245f6a7f0147faa1467fa5ebc0
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2024-07-03 20:09:56 -0400

    No more caching crashes.

--- source/backend/ImageCache.hx
+++ source/backend/ImageCache.hx
@@ -3,26 +3,38 @@ package backend;
 import flixel.graphics.FlxGraphic;
 import openfl.display.BitmapData;
 
-class ImageCache{
+class ImageCache {
 
     public static var cache:Map<String, FlxGraphic> = new Map<String, FlxGraphic>();
 
-    public static function add(path:String):Void{
-        
-        var data:FlxGraphic = FlxGraphic.fromBitmapData(GPUBitmap.create(path));
-        data.persist = true;
-        data.destroyOnNoUse = false;
-        //trace(cache);
+    public static function add(path:String):Void {
+        try {
+            var data:FlxGraphic = FlxGraphic.fromBitmapData(GPUBitmap.create(path));
+            data.persist = true;
+            data.destroyOnNoUse = false;
+            //trace(cache);
 
-        cache.set(path, data);
+            cache.set(path, data);
+        } catch (e:Dynamic) {
+            trace("Error adding image to cache: "+ e);
+        }
     }
 
-    public static function get(path:String):FlxGraphic{
-        return cache.get(path);
+    public static function get(path:String):FlxGraphic {
+        try {
+            return cache.get(path);
+        } catch (e:Dynamic) {
+            trace("Error getting image from cache:"+ e);
+            return null;
+        }
     }
 
-    public static function exists(path:String){
-        return cache.exists(path);
+    public static function exists(path:String):Bool {
+        try {
+            return cache.exists(path);
+        } catch (e:Dynamic) {
+            trace("Error checking if image exists in cache: "+ e);
+            return false;
+        }
     }
-
 }
