af0fd39bf61b9f924293ad642f0b2e36f56d30d4
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2024-11-15 10:43:55 -0500

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -9,6 +9,8 @@ haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
+haxelib install flixel-text-input 1.4.0
+haxelib install flixel-ui 2.6.1
 haxelib install hscript-iris 1.0.2
 haxelib install tjson 1.4.0
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
@@ -17,5 +19,8 @@ haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb
 haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc 70e7f5f3e76d526ac6fb8f0e6665efe7dfda589d
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis d5361037efa3a02c4ab20b5bd14ca11e7d00f519
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+haxelib git tentools https://github.com/TentaRJ/tentools.git
+haxelib git systools https://github.com/haya3218/systools
+haxelib run lime rebuild systools windows
 echo Finished!
 pause
